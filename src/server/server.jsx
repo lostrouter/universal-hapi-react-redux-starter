da4f7c5d9a10a531777599d047b4988d0c50013a
Michael Steinberg <steinberg.michael@outlook.com>
2015-11-01 17:33:04 -0800

    html and route to compiled js

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -7,33 +7,70 @@ import App from '../app';
 import React from 'react';
 import ReactDOM from 'react-dom/server'
 
-const Server = new Hapi.Server();
+const Server = new Hapi.Server(),
+	pageHead =
+		'<!DOCTYPE html>' +
+		'<html>' +
+		'<head>' +
+		'<title>bidchuck app</title>' +
+		'<link href="style.css" rel="stylesheet" />' +
+		'<meta content="text/html;charset=utf-8" http-equiv="Content-Type">' +
+		'<meta content="utf-8" http-equiv="encoding">' +
+		'</head>' +
+		'<body>' +
+		'<div id="mainContainer">',
+	pageFoot =
+		'</div>' +
+		'<script src="/public/app.min.js" type="text/javascript" />'
+		'</body>'
+		'</html>';
+
+
 
 Server.connection({ port: process.env.port || 3000 });
 
 Server.register(
-	{
-		register: Good,
-		options: {
-			reporters: [{
-				reporter: GoodConsole,
-				events: { log: '*',
-				response: '*' }
-			}]
-		}
-	},
+	[
+		{
+			register: Good,
+			options: {
+				reporters: [{
+					reporter: GoodConsole,
+					events: { log: '*',
+					response: '*' }
+				}]
+			}
+		},
+		Inert
+	],
 	(err) => {
 		if (err) {
 			throw err;
 		}
 
-		Server.route({
-			method: 'GET',
-			path: '/',
-			handler: (req, reply) => {
-				reply(ReactDOM.renderToString(<App />));
-			}
-		});
+		Server.route([
+			{
+				method: 'GET',
+				path: '/public/{file*}',
+				handler: {
+					directory: {
+						path: 'dist/public',
+						listing: false,
+						index: false
+					}
+				}
+			},
+			{
+				method: 'GET',
+				path: '/',
+				handler: (req, reply) => {
+					const body = pageHead +
+						ReactDOM.renderToString(<App />) +
+						pageFoot;
+					reply(body);
+				}
+			}]
+		);
 		Server.start(() => {
 			Server.log('info', 'Server running at: ' + Server.info.uri);
 		});
